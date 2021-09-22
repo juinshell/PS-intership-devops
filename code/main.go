59afd964db6a410d94ca9a1fbafcc76b53ef1878
husterdjx <76643896@qq.com>
2021-09-22 23:45:31 +0800

    refactor

---
+++ code/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+type Article struct{
+	Title string
+	Content string
+
+}
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK,"index.html",gin.H{
+			"title":"首页",
+		})
+	})
+	news := &Article{
+		Title: "intership get started!",
+		Content: "easy html",
+	}
+	r.GET("/news", func(c *gin.Context) {
+		c.HTML(http.StatusOK,"news.html",gin.H{
+			"title":"新闻页面",
+			"news":news, 
+		})
+	})
+	r.Run()
+}
