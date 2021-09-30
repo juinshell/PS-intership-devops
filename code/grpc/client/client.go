38d403e58d76f542beddba500d8dc794a6832ccc
husterdjx <76643896@qq.com>
2021-09-30 15:40:32 +0800

    fix:client.go

--- code/grpc/client/client.go
+++ code/grpc/client/client.go
@@ -2,59 +2,48 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
-	"os"
-	"sync"
-	"time"
 	pb "grpc/proto"
+	"os"
+	"time"
+
 	"google.golang.org/grpc"
 )
 const (
 	defaultName = "world"
 	address     = "hello-grpc-server-service:80"
+	//address     = ":50053"
 )
-var sig = 0
-var mu sync.Mutex
+
 func echo(wr http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	sig = 1
-	mu.Unlock()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewMessageSenderClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	rr, err := c.Send(ctx, &pb.MessageRequest{SaySomething: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", rr.GetResponseSomething())
 }	
 func main() {
-	go func(){
-		http.HandleFunc("/", echo)
-		err := http.ListenAndServe(":50060", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	for ; ; {
-		if sig == 1 {
-			// Set up a connection to the server.
-			conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-			if err != nil {
-				log.Fatalf("did not connect: %v", err)
-			}
-			c := pb.NewMessageSenderClient(conn)
-
-			// Contact the server and print out its response.
-			name := defaultName
-			if len(os.Args) > 1 {
-				name = os.Args[1]
-			}
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-			r, err := c.Send(ctx, &pb.MessageRequest{SaySomething: name})
-			if err != nil {
-				log.Fatalf("could not greet: %v", err)
-			}
-			log.Printf("Greeting: %s", r.GetResponseSomething())
-			mu.Lock()
-			sig = 0
-			conn.Close()
-			cancel()
-			mu.Unlock()
-		}
-	}
+	http.HandleFunc("/", echo)
+	err := http.ListenAndServe(":50060", nil)
+	if err != nil {
+		log.Fatal(err)
+	}	
 }
